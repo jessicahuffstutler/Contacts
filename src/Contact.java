780f9f4b6718a4cb920b84fc72949204163aa36f
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-10-13 10:21:36 -0400

    add test code to practice constructors and data structures

--- src/Contact.java
+++ src/Contact.java
@@ -7,27 +7,8 @@ public class Contact {
     public String email;
     public int age;
 
-    public Contact() { //Constructor; no arguments (whats inside the parenthesis)
-        this.name = "Jessica Huffstutler";
-        this.email = "jessica.huffstutler@gmail.com";
-    }
-
-    public Contact(String name, String address, String email, int age) { //Constructor with 4 different arguments
-        this.name = name;
-        this.address = address;
-        this.email = email;
+    public Contact(String name, int age) {
+        this.name = name; //assigns a value to a variable. this.name refers to the "pubilc string name", the second name refers to the "String Name" from the parenthesis above.
         this.age = age;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String newName){
-        this.name = newName; //this."name" refers to the first instance of name in the code,(field) not after void (argument), which we changed to newName to reduce confusion
-    }
-
-    public static boolean isValidName(String name) { //static method
-        return name.length() != 0;
     }
 }
---
+++ src/Contacts.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by jessicahuffstutler on 10/13/15.
+ */
+public class Contacts {
+    public static void main(String[] args) {
+        Contact bob = new Contact("Bob", 30); //make use of the constructor from "Contact.java"
+        Contact alice = new Contact ("Alice", 29);
+        Contact charlie = new Contact ("Charlie", 25);
+
+        ArrayList<Contact> contacts = new ArrayList<>(); //we are storing contact objects in here
+        contacts.add(bob);
+        contacts.add(alice);
+        contacts.add(charlie);
+
+        ArrayList<Contact> contactsUnderThirty = new ArrayList();
+
+        Contact contactToFind = null;
+        for (Contact c : contacts) { //for loops are for looping over collections, left side of colon is temporary variable and right side is our list "contacts", think of colon as the word "in"
+            if (c.age < 30) { //searching for people with age less than 30
+                contactsUnderThirty.add(c);
+            }
+        }
+
+        System.out.println(contactsUnderThirty);
+
+        HashMap<String, Contact> contactMap = new HashMap();
+        contactMap.put("Bob", bob); //putting contacts in the hashmap
+        contactMap.put("Alice", alice);
+        contactMap.put("Charlie", charlie);
+
+        //contactToFind = contactMap.get("Alice");
+
+        if (contactToFind == null) {
+            System.out.println("Couldn't find contact.");
+        } else {
+            System.out.println("Found contact");
+        }
+    }
+}
